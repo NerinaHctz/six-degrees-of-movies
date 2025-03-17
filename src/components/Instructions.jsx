4b7d51ef83ce0f4c1c40c442023c5fc13b8b00d9
Nerina Castillo <nerina.cgomez@gmail.com>
2025-03-17 21:08:31 +0100

    Add styles on gameboard component

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -8,11 +8,11 @@ const Instructions = ({ onStart }) => {
     return <Container className='instructions'>
         <Title className='instructions-title' level={2}>Instrucciones</Title>
         <ul>
-            <li>Haz clic en los dados para seleccionar dos actores al azar.</li>
-            <li>Pulsa en el actor de la izquierda para ver las películas en las que aparece.</li>
-            <li>Haz clic en una película de la lista para ver los actores que han actuado en ella.</li>
-            <li>Haz clic en un actor de la lista para ver sus películas.</li>
-            <li>Encuentra el camino más corto entre el actor de la izquierda y el de la derecha.</li>
+            <li><img src='/icon/dices_white.png' alt='Dados' />Haz clic en los dados para seleccionar dos actores al azar.</li>
+            <li><img src='/icon/actress.png' alt='Actriz' />Pulsa en el actor de la izquierda para ver las películas en las que aparece.</li>
+            <li><img src='/icon/film.png' alt='Pelicula' />Haz clic en una película de la lista para ver los actores que han actuado en ella.</li>
+            <li><img src='/icon/actor.png' alt='Actor' />Haz clic en un actor de la lista para ver sus películas.</li>
+            <li><img src='/icon/nexus.png' alt='Nexo' />Encuentra el camino más corto entre el actor de la izquierda y el de la derecha.</li>
         </ul>
         <Button className='instructions-button' onClick={onStart}>Jugar</Button>
     </Container>
